9c666abed8b30c3df0871bf809777c416f9bd094
Julie Havlickova <jhavlick@c1r2s7.42prague.com>
2023-09-25 18:50:19 +0200

    whasap

--- libft.h
+++ libft.h
@@ -6,20 +6,25 @@
 /*   By: jhavlick <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 18:04:48 by jhavlick          #+#    #+#             */
-/*   Updated: 2023/09/25 18:05:28 by jhavlick         ###   ########.fr       */
+/*   Updated: 2023/09/25 18:47:54 by jhavlick         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
-# include <stdio.h>
+# include <stdarg.h>
 
-int	ft_isalnum(int c);
 int	ft_isalpha(int c);
-int	ft_isascii(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
