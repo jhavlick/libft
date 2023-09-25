64c46e18dae7419edb3222759f27f4df830b81d2
Julie Havlickova <jhavlick@c1r2s7.42prague.com>
2023-09-25 19:11:33 +0200

    upload again

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jhavlick <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 18:04:48 by jhavlick          #+#    #+#             */
-/*   Updated: 2023/09/25 18:47:54 by jhavlick         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:08:30 by jhavlick         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 
