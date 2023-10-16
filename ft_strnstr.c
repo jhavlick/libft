15acdf4228e5c727d86c30f99ede724e2e4816d0
Julie Havlickova <jhavlick@c3r4s1.42prague.com>
2023-10-16 14:31:40 +0200

    Here we go again

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,34 @@
 /*   By: jhavlick <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:51:59 by jhavlick          #+#    #+#             */
-/*   Updated: 2023/10/12 17:54:18 by jhavlick         ###   ########.fr       */
+/*   Updated: 2023/10/16 13:10:01 by jhavlick         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	if (!*needle)
+	{
+		return ((char *)haystack);
+	}
+	while (haystack[i] && i < n)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < n && needle [j])
+		{
+			j++;
+		}
+		if (!needle[j])
+		{
+			return ((char *)(haystack + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
