818da407ddb953306ef41a437c67ce742efb303d
Julie Havlíčková <julie@Julie-MacBook-Air.local>
2023-10-19 16:39:08 +0200

    working while introduction meeting of pisciners

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,34 @@
 /*   By: jhavlick <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 15:24:17 by jhavlick          #+#    #+#             */
-/*   Updated: 2023/10/19 15:27:14 by jhavlick         ###   ########.fr       */
+/*   Updated: 2023/10/19 16:38:17 by jhavlick         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-{}
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(*s)*(len + 1));
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	retrun (str);
+}
